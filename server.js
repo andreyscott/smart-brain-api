5a0cdc670f72de113b82446d67a724fd3588a6bf
andreyscott <62455784+andreyscott@users.noreply.github.com>
2020-11-10 10:41:47 +0100

    la

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const database = {
 
 app.get('/', (req, res) => {
 	console.log(req.body)
-	res.json("this is working");
+	res.json(database.users);
 })
 
 app.post('/signin', (req, res) => {
@@ -39,6 +39,30 @@ app.post('/signin', (req, res) => {
 	} else {
 		res.status(400).json("erroring loading");
 	}
+})
+
+app.post('/register', (req, res) => {
+	const {name, email, password } = req.body; 
+	database.users.push({
+		id: "126",
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			res.json(user);
+		} else {
+			res.status(404).json("no user found")
+		}
+	})
 })
 
 app.listen(3000, ()=> {
