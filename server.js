1807eda70db4313f3b5e26855e1e0bcca4c51280
andreyscott <62455784+andreyscott@users.noreply.github.com>
2020-11-11 10:12:04 +0100

    lala

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -11,7 +12,6 @@ const database = {
 		id: "123",
 		name: "AnDrey",
 		email: "andreyscott@gmail.com",
-		password: "password",
 		entries: 0,
 		joined: new Date()
 	},
@@ -19,11 +19,17 @@ const database = {
 		id: "124",
 		name: "AnDrew",
 		email: "andrewking@gmail.com",
-		password: "incorret",
 		entries: 0,
 		joined: new Date()
     }
-  ]
+  ],
+  login: [
+  {
+  	id: "987",
+  	hash: "",
+  	email: 'john@gmail.com'
+  }
+ ]
 }
 
 
@@ -33,6 +39,13 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+bcrypt.compare("password", '$2a$10$OoAWRi/U9t5WvEw5PDjlbeze/M0taJrpDIKd4FFc7kfULilGlbc8O', function(err, res) {
+ console.log("first password", res)
+});
+bcrypt.compare("veggies", '$2a$10$OoAWRi/U9t5WvEw5PDjlbeze/M0taJrpDIKd4FFc7kfULilGlbc8O', function(err, res) {
+ console.log("second guess", res)
+});
+
 	if (req.body.email === database.users[0].email &&
 		req.body.password === database.users[0].password){
       res.json('sigining in successful')
@@ -43,7 +56,7 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const {name, email, password } = req.body; 
-	database.users.push({
+  		database.users.push({
 		id: "126",
 		name: name,
 		email: email,
